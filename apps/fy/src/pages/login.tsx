805e7c813be1c6a4b7e94671890da5bdaae16fc0
Maciej Gorczyn <gorczynmaciej@gmail.com>
2024-10-20 17:45:28 +0200

    feat: demo ui improvements

--- apps/fy/src/pages/login.tsx
+++ apps/fy/src/pages/login.tsx
@@ -15,62 +15,77 @@ export const LoginDemoPage = () => {
     }
   );
   return (
-    <div className="flex flex-col gap-5">
-      <div className="card">
-        <div className="card-body">
-          <div className="mx-auto flex w-full max-w-sm flex-col gap-6">
-            <div className="flex flex-col items-center">
-              <h1 className="text-3xl font-semibold">Sign In</h1>
-              <p className="text-sm">Sign in to access your account</p>
-            </div>
-            <form className="form-group" {...login.form}>
-              <div className="form-field">
-                <label className="form-label">Email address</label>
-                <input placeholder="Type here" type="email" className={cn('input max-w-full', { 'input-error': login.get('login').errors?.email })} {...login.field('login')} />
-                {login.get('login').errors?.email && (
-                  <label className="form-label">
-                    <span className="form-label-alt text-error">Please enter a valid email.</span>
-                  </label>
-                )}
-              </div>
-              <div className="form-field">
-                <label className="form-label">Password</label>
-                <div className="form-control">
-                  <input
-                    placeholder="Type here"
-                    type="password"
-                    className="input max-w-full"
-                    {...login.field('password')}
-                  />
+    <div className="flex max-w-full w-full card bg-backgroundSecondary">
+      <div className="card-body flex flex-col gap-4 w-full">
+        <div>
+          <h1 className="text-3xl font-semibold mb-3">Demo</h1>
+          <div className="card">
+            <div className="card-body">
+              <div className="mx-auto flex w-full max-w-sm flex-col gap-6">
+                <div className="flex flex-col items-center">
+                  <h1 className="text-3xl font-semibold">Sign In</h1>
+                  <p className="text-sm">Sign in to access your account</p>
                 </div>
-              </div>
-              <div className="form-field">
-                <div className="form-control justify-between">
-                  <div className="flex gap-2">
-                    <input className="checkbox" {...login.checkbox('rememberMe')} />
-                    <a href="#">Remember me</a>
+                <form className="form-group" {...login.form}>
+                  <div className="form-field">
+                    <label className="form-label">Email address</label>
+                    <input
+                      placeholder="Type here"
+                      type="email"
+                      className={cn('input max-w-full', { 'input-error': login.get('login').errors?.email })}
+                      {...login.field('login')}
+                    />
+                    {login.get('login').errors?.email && (
+                      <label className="form-label">
+                        <span className="form-label-alt text-error">Please enter a valid email.</span>
+                      </label>
+                    )}
                   </div>
-                  <label className="form-label">
-                    <a className="link link-underline-hover link-primary text-sm">Forgot your password?</a>
-                  </label>
-                </div>
-              </div>
-              <div className="form-field pt-5">
-                <div className="form-control justify-between">
-                  <input type="submit" className="btn btn-primary w-full" value={'Sign in'} />
-                </div>
-              </div>
+                  <div className="form-field">
+                    <label className="form-label">Password</label>
+                    <div className="form-control">
+                      <input
+                        placeholder="Type here"
+                        type="password"
+                        className="input max-w-full"
+                        {...login.field('password')}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-field">
+                    <div className="form-control justify-between">
+                      <div className="flex gap-2">
+                        <input className="checkbox" {...login.checkbox('rememberMe')} />
+                        <a href="#">Remember me</a>
+                      </div>
+                      <label className="form-label">
+                        <a className="link link-underline-hover link-primary text-sm">Forgot your password?</a>
+                      </label>
+                    </div>
+                  </div>
+                  <div className="form-field pt-5">
+                    <div className="form-control justify-between">
+                      <input type="submit" className="btn btn-primary w-full" value={'Sign in'} />
+                    </div>
+                  </div>
 
-              <div className="form-field">
-                <div className="form-control justify-center">
-                  <a className="link link-underline-hover link-primary text-sm">Don't have an account yet? Sign up.</a>
-                </div>
+                  <div className="form-field">
+                    <div className="form-control justify-center">
+                      <a className="link link-underline-hover link-primary text-sm">
+                        Don't have an account yet? Sign up.
+                      </a>
+                    </div>
+                  </div>
+                </form>
               </div>
-            </form>
+            </div>
           </div>
         </div>
+        <div>
+          <h1 className="text-3xl font-semibold mb-3">Output</h1>
+          <code className="kbd block whitespace-pre overflow-hidden">{JSON.stringify(login.all, null, 4)}</code>
+        </div>
       </div>
-      <code className="kbd block whitespace-pre overflow-x-scroll">{JSON.stringify(login.all, null, 4)}</code>
     </div>
   );
 };
